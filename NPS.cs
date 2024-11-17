a56892fa4ca35603defb59b3f40236bd1c506061
Gleb718 <glebp6919@gmail.com>
2024-11-17 15:49:27 +1000

    Add files via upload

---
+++ NPS.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPS : MonoBehaviour
+{
+    //Здоровье NPS
+    public int health = 5;
+
+    //Уровень NPS
+    public int level = 1;
+
+    //Скорость NPS
+    public float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position; 
+        newPosition.z +=
+speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
